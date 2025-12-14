88ccd4c07011bb8d03e50358cd58baa58e2882b2
William Strothe <95983821+wwstrothe@users.noreply.github.com>
2025-12-14 09:46:08 -0700

    Remove app (#6)
    * remove william-strothe app
    
    * remove william-strothe app from deploy
    
    * remove initial app, trying new commands
    
    * added portfolio app via Nx Console

--- apps/portfolio-e2e/playwright.config.ts
+++ apps/portfolio-e2e/playwright.config.ts
@@ -3,7 +3,7 @@ import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
 
 // For CI, you may want to set BASE_URL to the deployed application.
-const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
+const baseURL = process.env['BASE_URL'] || 'http://localhost:4300';
 
 /**
  * Read environment variables from file.
@@ -24,8 +24,8 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm exec nx run portfolio:serve',
-    url: 'http://localhost:4200',
+    command: 'pnpm exec nx run William Strothe Portfolio:serve',
+    url: 'http://localhost:4300',
     reuseExistingServer: true,
     cwd: workspaceRoot,
   },
