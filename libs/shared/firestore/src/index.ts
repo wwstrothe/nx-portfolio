453b2ffc7572590d569500f07c3cf3bf07b4e3bb
wwstrothe <95983821+wwstrothe@users.noreply.github.com>
2025-12-14 20:36:42 -0700

    libs(firestore): add collection-tools helper with signal-based CRUD and tests

--- libs/shared/firestore/src/index.ts
+++ libs/shared/firestore/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/adapters/web-firestore.adapter';
 export * from './lib/batch';
+export * from './lib/collection-tools';
 export * from './lib/crud';
 export * from './lib/types';
-
